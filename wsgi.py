53eefa030172913003a1e6fd362bb3dbb8054dfe
BuildTools <raar518@aucklanduni.ac.nz>
2022-09-03 21:46:07 +1200

    testing

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,8 @@
 from music import create_app
 
 app = create_app()
-
+# if app.debug == True:
+    # print(app.debug)    # app.run(debug=True)
+# app.debug = True
 if __name__ == "__main__":
     app.run(host='localhost', port=5000, threaded=False)
