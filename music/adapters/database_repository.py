4fb9bb507746845dd9897e5c64239cb1019da0b0
BuildTools <raar518@aucklanduni.ac.nz>
2022-10-05 02:34:07 +1300

    session manager

---
+++ music/adapters/database_repository.py
@@ -0,0 +1,54 @@
+from datetime import date
+from typing import List
+
+from sqlalchemy import desc, asc
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+
+from sqlalchemy.orm import scoped_session
+
+from music.domainmodel.user import User
+from music.domainmodel.track import Track, Review
+from music.domainmodel.artist import Artist
+
+from music.adapters.repository import AbstractRepository
+class SqlAlchemyRepository(AbstractRepository):
+
+    def __init__(self, session_factory):
+        self._session_cm = SessionContextManager(session_factory)
+
+    def close_session(self):
+        self._session_cm.close_current_session()
+
+    def reset_session(self):
+        self._session_cm.reset_session()
+
+class SessionContextManager:
+    def __init__(self, session_factory):
+        self.__session_factory = session_factory
+        self.__session = scoped_session(self.__session_factory)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.rollback()
+
+    @property
+    def session(self):
+        return self.__session
+
+    def commit(self):
+        self.__session.commit()
+
+    def rollback(self):
+        self.__session.rollback()
+
+    def reset_session(self):
+        # this method can be used e.g. to allow Flask to start a new session for each http request,
+        # via the 'before_request' callback
+        self.close_current_session()
+        self.__session = scoped_session(self.__session_factory)
+
+    def close_current_session(self):
+        if not self.__session is None:
+            self.__session.close()
