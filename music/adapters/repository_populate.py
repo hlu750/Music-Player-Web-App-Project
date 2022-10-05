5dd2201d4c5dde17041da36b95c40b8331714834
BuildTools <raar518@aucklanduni.ac.nz>
2022-10-05 18:00:52 +1300

    stuff

---
+++ music/adapters/repository_populate.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from music.adapters.repository import AbstractRepository
+from music.adapters.csvdatareader import load_reviews, load_users, load_tracks
+
+
+def populate(data_path: Path, repo: AbstractRepository, database_mode: bool):
+    # Load articles and tags into the repository.
+    load_articles_and_tags(data_path, repo, database_mode)
+
+    # Load users into the repository.
+    users = load_users(data_path, repo)
+
+    # Load comments into the repository.
+    load_comments(data_path, repo, users)
