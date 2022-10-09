e3d6bd6035c7c80ec3d94e6aa9c1b84f2b82d66d
hlu750 <hlu750@aucklanduni.ac.nz>
2022-10-10 03:31:13 +1300

    orm tests

--- tests_db/unit/test_orm.py
+++ tests_db/unit/test_orm.py
@@ -56,22 +56,22 @@ def insert_track_genre_associations(empty_session, track_key, genre_keys):
         empty_session.execute(stmt, {'track_id': track_key, 'genre_id': genre_key})
 
 
-# def insert_commented_track(empty_session):
-#     track_key = insert_track(empty_session)
-#     user_key = insert_user(empty_session)
+def insert_commented_track(empty_session):
+    track_key = insert_track(empty_session)
+    user_key = insert_user(empty_session)
 
-#     timestamp_1 = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-#     timestamp_2 = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    timestamp_1 = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    timestamp_2 = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-#     empty_session.execute(
-#         'INSERT INTO comments (user_id, track_id, comment, timestamp) VALUES '
-#         '(:user_id, :track_id, "Comment 1", :timestamp_1),'
-#         '(:user_id, :track_id, "Comment 2", :timestamp_2)',
-#         {'user_id': user_key, 'track_id': track_key, 'timestamp_1': timestamp_1, 'timestamp_2': timestamp_2}
-#     )
+    empty_session.execute(
+        'INSERT INTO comments (user_id, track_id, comment, timestamp) VALUES '
+        '(:user_id, :track_id, "Comment 1", :timestamp_1),'
+        '(:user_id, :track_id, "Comment 2", :timestamp_2)',
+        {'user_id': user_key, 'track_id': track_key, 'timestamp_1': timestamp_1, 'timestamp_2': timestamp_2}
+    )
 
-#     row = empty_session.execute('SELECT id from tracks').fetchone()
-#     return row[0]
+    row = empty_session.execute('SELECT id from tracks').fetchone()
+    return row[0]
 
 
 def make_track():
@@ -89,160 +89,160 @@ def make_genre():
     return genre
 
 
-# def test_loading_of_users(empty_session):
-#     users = list()
-#     users.append(("Andrew", "1234"))
-#     users.append(("Cindy", "1111"))
-#     insert_users(empty_session, users)
+def test_loading_of_users(empty_session):
+    users = list()
+    users.append((4, "Andrew", "1234"))
+    users.append((5, "Cindy", "1111"))
+    insert_users(empty_session, users)
 
-#     expected = [
-#         User("Andrew", "1234"),
-#         User("Cindy", "999")
-#     ]
-#     assert empty_session.query(User).all() == expected
+    expected = [
+        User(4, "Andrew", "1234"),
+        User(5, "Cindy", "999")
+    ]
+    assert empty_session.query(User).all() == expected
 
-# def test_saving_of_users(empty_session):
-#     user = make_user()
-#     empty_session.add(user)
-#     empty_session.commit()
+def test_saving_of_users(empty_session):
+    user = make_user()
+    empty_session.add(user)
+    empty_session.commit()
 
-#     rows = list(empty_session.execute('SELECT user_name, password FROM users'))
-#     assert rows == [("Andrew", "111")]
-
-
-# def test_saving_of_users_with_common_user_name(empty_session):
-#     insert_user(empty_session, ("Andrew", "1234"))
-#     empty_session.commit()
-
-#     with pytest.raises(IntegrityError):
-#         user = User("Andrew", "111")
-#         empty_session.add(user)
-#         empty_session.commit()
+    rows = list(empty_session.execute('SELECT user_name, password FROM users'))
+    assert rows == [(4, "Andrew", "111")]
 
 
-# def test_loading_of_track(empty_session):
-#     track_key = insert_track(empty_session)
-#     expected_track = make_track()
-#     fetched_track = empty_session.query(track).one()
+def test_saving_of_users_with_common_user_name(empty_session):
+    insert_user(empty_session, (4, "Andrew", "1234"))
+    empty_session.commit()
 
-#     assert expected_track == fetched_track
-#     assert track_key == fetched_track.id
-
-
-# def test_loading_of_genreged_track(empty_session):
-#     track_key = insert_track(empty_session)
-#     genre_keys = insert_genres(empty_session)
-#     insert_track_genre_associations(empty_session, track_key, genre_keys)
-
-#     track = empty_session.query(track).get(track_key)
-#     genres = [empty_session.query(genre).get(key) for key in genre_keys]
-
-#     for genre in genres:
-#         assert track.is_genreged_by(genre)
-#         assert genre.is_applied_to(track)
+    with pytest.raises(IntegrityError):
+        user = User(4, "Andrew", "111")
+        empty_session.add(user)
+        empty_session.commit()
 
 
-# def test_loading_of_commented_track(empty_session):
-#     insert_commented_track(empty_session)
+def test_loading_of_track(empty_session):
+    track_key = insert_track(empty_session)
+    expected_track = make_track()
+    fetched_track = empty_session.query(Track).one()
 
-#     rows = empty_session.query(track).all()
-#     track = rows[0]
-
-#     for comment in track.comments:
-#         assert comment.track is track
-
-
-# def test_saving_of_comment(empty_session):
-#     track_key = insert_track(empty_session)
-#     user_key = insert_user(empty_session, ("Andrew", "1234"))
-
-#     rows = empty_session.query(track).all()
-#     track = rows[0]
-#     user = empty_session.query(User).filter(User._User__user_name == "Andrew").one()
-
-#     # Create a new Comment that is bidirectionally linked with the User and track.
-#     comment_text = "Some comment text."
-#     comment = make_comment(comment_text, user, track)
-
-#     # Note: if the bidirectional links between the new Comment and the User and
-#     # track objects hadn't been established in memory, they would exist following
-#     # committing the addition of the Comment to the database.
-#     empty_session.add(comment)
-#     empty_session.commit()
-
-#     rows = list(empty_session.execute('SELECT user_id, track_id, comment FROM comments'))
-
-#     assert rows == [(user_key, track_key, comment_text)]
+    assert expected_track == fetched_track
+    assert track_key == fetched_track.id
 
 
-# def test_saving_of_track(empty_session):
-#     track = make_track()
-#     empty_session.add(track)
-#     empty_session.commit()
+def test_loading_of_genreged_track(empty_session):
+    track_key = insert_track(empty_session)
+    genre_keys = insert_genres(empty_session)
+    insert_track_genre_associations(empty_session, track_key, genre_keys)
 
-#     rows = list(empty_session.execute('SELECT date, title, first_paragraph, hyperlink, image_hyperlink FROM tracks'))
-#     date = track_date.isoformat()
-#     assert rows == [(date,
-#                      "Coronavirus: First case of virus in New Zealand",
-#                      "The first case of coronavirus has been confirmed in New Zealand  and authorities are now scrambling to track down people who may have come into contact with the patient.",
-#                      "https://www.stuff.co.nz/national/health/119899280/ministry-of-health-gives-latest-update-on-novel-coronavirus",
-#                      "https://resources.stuff.co.nz/content/dam/images/1/z/e/3/w/n/image.related.StuffLandscapeSixteenByNine.1240x700.1zduvk.png/1583369866749.jpg"
-#                      )]
+    track = empty_session.query(track).get(track_key)
+    genres = [empty_session.query(genre).get(key) for key in genre_keys]
+
+    for genre in genres:
+        assert track.is_genreged_by(genre)
+        assert genre.is_applied_to(track)
 
 
-# def test_saving_genreged_track(empty_session):
-#     track = make_track()
-#     genre = make_genre()
+def test_loading_of_commented_track(empty_session):
+    insert_commented_track(empty_session)
 
-#     # Establish the bidirectional relationship between the track and the genre.
-#     make_genre_association(track, genre)
+    rows = empty_session.query(track).all()
+    track = rows[0]
 
-#     # Persist the track (and genre).
-#     # Note: it doesn't matter whether we add the genre or the track. They are connected
-#     # bidirectionally, so persisting either one will persist the other.
-#     empty_session.add(track)
-#     empty_session.commit()
-
-#     # Test test_saving_of_track() checks for insertion into the tracks table.
-#     rows = list(empty_session.execute('SELECT id FROM tracks'))
-#     track_key = rows[0][0]
-
-#     # Check that the genres table has a new record.
-#     rows = list(empty_session.execute('SELECT id, genre_name FROM genres'))
-#     genre_key = rows[0][0]
-#     assert rows[0][1] == "News"
-
-#     # Check that the track_genres table has a new record.
-#     rows = list(empty_session.execute('SELECT track_id, genre_id from track_genres'))
-#     track_foreign_key = rows[0][0]
-#     genre_foreign_key = rows[0][1]
-
-#     assert track_key == track_foreign_key
-#     assert genre_key == genre_foreign_key
+    for comment in track.comments:
+        assert comment.track is track
 
 
-# def test_save_commented_track(empty_session):
-#     # Create track User objects.
-#     track = make_track()
-#     user = make_user()
+def test_saving_of_comment(empty_session):
+    track_key = insert_track(empty_session)
+    user_key = insert_user(empty_session, (4, "Andrew", "1234"))
 
-#     # Create a new Comment that is bidirectionally linked with the User and track.
-#     comment_text = "Some comment text."
-#     comment = make_comment(comment_text, user, track)
+    rows = empty_session.query(track).all()
+    track = rows[0]
+    user = empty_session.query(User).filter(User._User__user_name == "Andrew").one()
 
-#     # Save the new track.
-#     empty_session.add(track)
-#     empty_session.commit()
+    # Create a new Comment that is bidirectionally linked with the User and track.
+    comment_text = "Some comment text."
+    comment = make_comment(comment_text, user, track)
 
-#     # Test test_saving_of_track() checks for insertion into the tracks table.
-#     rows = list(empty_session.execute('SELECT id FROM tracks'))
-#     track_key = rows[0][0]
+    # Note: if the bidirectional links between the new Comment and the User and
+    # track objects hadn't been established in memory, they would exist following
+    # committing the addition of the Comment to the database.
+    empty_session.add(comment)
+    empty_session.commit()
 
-#     # Test test_saving_of_users() checks for insertion into the users table.
-#     rows = list(empty_session.execute('SELECT id FROM users'))
-#     user_key = rows[0][0]
+    rows = list(empty_session.execute('SELECT user_id, track_id, comment FROM comments'))
 
-#     # Check that the comments table has a new record that links to the tracks and users
-#     # tables.
-#     rows = list(empty_session.execute('SELECT user_id, track_id, comment FROM comments'))
-#     assert rows == [(user_key, track_key, comment_text)]
+    assert rows == [(user_key, track_key, comment_text)]
+
+
+def test_saving_of_track(empty_session):
+    track = make_track()
+    empty_session.add(track)
+    empty_session.commit()
+
+    rows = list(empty_session.execute('SELECT date, title, first_paragraph, hyperlink, image_hyperlink FROM tracks'))
+    date = track_date.isoformat()
+    assert rows == [(date,
+                     "Coronavirus: First case of virus in New Zealand",
+                     "The first case of coronavirus has been confirmed in New Zealand  and authorities are now scrambling to track down people who may have come into contact with the patient.",
+                     "https://www.stuff.co.nz/national/health/119899280/ministry-of-health-gives-latest-update-on-novel-coronavirus",
+                     "https://resources.stuff.co.nz/content/dam/images/1/z/e/3/w/n/image.related.StuffLandscapeSixteenByNine.1240x700.1zduvk.png/1583369866749.jpg"
+                     )]
+
+
+def test_saving_genreged_track(empty_session):
+    track = make_track()
+    genre = make_genre()
+
+    # Establish the bidirectional relationship between the track and the genre.
+    make_genre_association(track, genre)
+
+    # Persist the track (and genre).
+    # Note: it doesn't matter whether we add the genre or the track. They are connected
+    # bidirectionally, so persisting either one will persist the other.
+    empty_session.add(track)
+    empty_session.commit()
+
+    # Test test_saving_of_track() checks for insertion into the tracks table.
+    rows = list(empty_session.execute('SELECT id FROM tracks'))
+    track_key = rows[0][0]
+
+    # Check that the genres table has a new record.
+    rows = list(empty_session.execute('SELECT id, genre_name FROM genres'))
+    genre_key = rows[0][0]
+    assert rows[0][1] == "News"
+
+    # Check that the track_genres table has a new record.
+    rows = list(empty_session.execute('SELECT track_id, genre_id from track_genres'))
+    track_foreign_key = rows[0][0]
+    genre_foreign_key = rows[0][1]
+
+    assert track_key == track_foreign_key
+    assert genre_key == genre_foreign_key
+
+
+def test_save_commented_track(empty_session):
+    # Create track User objects.
+    track = make_track()
+    user = make_user()
+
+    # Create a new Comment that is bidirectionally linked with the User and track.
+    comment_text = "Some comment text."
+    comment = make_comment(comment_text, user, track)
+
+    # Save the new track.
+    empty_session.add(track)
+    empty_session.commit()
+
+    # Test test_saving_of_track() checks for insertion into the tracks table.
+    rows = list(empty_session.execute('SELECT id FROM tracks'))
+    track_key = rows[0][0]
+
+    # Test test_saving_of_users() checks for insertion into the users table.
+    rows = list(empty_session.execute('SELECT id FROM users'))
+    user_key = rows[0][0]
+
+    # Check that the comments table has a new record that links to the tracks and users
+    # tables.
+    rows = list(empty_session.execute('SELECT user_id, track_id, comment FROM comments'))
+    assert rows == [(user_key, track_key, comment_text)]
